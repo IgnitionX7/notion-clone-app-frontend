5c1396aa041d776b2f035ed95d042e5bdaa65b49
Shan Amin <robin_zill@yahoo.com>
2024-12-14 21:15:13 +0500

    added liveblocks pointer functionality

--- app/doc/[id]/page.tsx
+++ app/doc/[id]/page.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import Document from "@/components/Document";
+import { use } from "react";
 
-function DocumentPage({
-  params: { id },
+export default function DocumentPage({
+  params: paramsPromise,
 }: {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 }) {
+  const params = use(paramsPromise); // Unwrap the promise
+  const { id } = params;
+
   return (
     <div className="flex flex-col flex-1 min-h-screen">
       <Document id={id} />
     </div>
   );
 }
-export default DocumentPage;
